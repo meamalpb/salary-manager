5c0e47349a09a65a81155747d3122ad3dc990c97
Amal PB <321amalpb.com@gmail.com>
2026-04-09 17:21:40 +0530

    additional tests and code formatting setup

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -14,6 +14,18 @@ module Backend
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: true
+
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
+
     config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
